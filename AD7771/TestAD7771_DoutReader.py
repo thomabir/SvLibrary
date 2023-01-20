cbc75554ca2b391ff558512e08b204db75fcd3c4
Thomas Birbacher <thomabir@phys.ethz.ch>
2023-01-20 16:26:37 +0100

    Clean up

--- AD7771/TestAD7771_DoutReader.py
+++ AD7771/TestAD7771_DoutReader.py
@@ -1,11 +1,9 @@
+from cgi import test
+
 import cocotb
 import numpy as np
-
-from cgi import test
 from cocotb.clock import Clock
-from cocotb.triggers import FallingEdge
-from cocotb.triggers import RisingEdge
-from cocotb.triggers import Timer
+from cocotb.triggers import FallingEdge, RisingEdge, Timer
 
 np.random.seed(0)
 
@@ -42,17 +40,12 @@ async def ReadSingleMessage(dut, msg):
     await FallingEdge(dut.clk_i)  # wait for the state machine to finish
     await FallingEdge(dut.clk_i)
 
-    assert (
-        expect_ch1 == dut.ch1_o.value
-    ), f"Wrong data received: received {dut.ch1_o}, but expected {expect_ch1}"
-    assert (
-        expect_ch2 == dut.ch2_o.value
-    ), f"Wrong data received: received {dut.ch2_o}, but expected {expect_ch2}"
+    assert expect_ch1 == dut.ch1_o.value, f"Wrong data received: received {dut.ch1_o}, but expected {expect_ch1}"
+    assert expect_ch2 == dut.ch2_o.value, f"Wrong data received: received {dut.ch2_o}, but expected {expect_ch2}"
 
 
 @cocotb.test()
 async def ReadWriteTest(dut):
-
     # system clock of 20 ns, and a DCLK of 8 MHz = 125 ns
     clock = Clock(dut.clk_i, 20, units="ns")  # 20 ns = 50 MHz
     adc_clock = Clock(dut.dclk, 126, units="ns")  # 125 ns = 8 MHz
@@ -74,7 +67,6 @@ async def ReadWriteTest(dut):
     ch2_msgs = np.random.randint(low=0, high=2**24 - 1, size=n_test)
 
     for ch1_msg, ch2_msg in zip(ch1_msgs, ch2_msgs):
-
         # convert the messages to a 64 bit integer
         ch1_msg = bin(ch1_msg)[2:].zfill(24)
         ch2_msg = bin(ch2_msg)[2:].zfill(24)
